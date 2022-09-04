e61d90f64702821811c21f370e3fb998966e55c3
Astrid Pulse <astridcpulse@gmail.com>
2022-09-04 10:59:28 -0500

    added comments and formatted code for readability

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -12,7 +12,9 @@ for (let i=0; i<4; i++) {
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
+
 console.log('count from 0 to 5');
+
 for (let i=0; i<=5; i++){ //we need to change the condition for the loop running to be i<=5, this will allow us to count up to and including 5
   console.log(i);
 }
@@ -21,14 +23,16 @@ for (let i=0; i<=5; i++){ //we need to change the condition for the loop running
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-for (let i=3; i<=5; i++){
+
+for (let i=3; i<=5; i++){ //we need to change the starting index to three so the loop starts counting at 3 and up to 5 
   console.log(i);
 }
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-for (let i=2; i<=10; i++){
-  if (i % 2 === 0){
+
+for (let i=2; i<=10; i++){ // we change the starting index to two and the conditional to less that or equal to 10
+  if (i % 2 === 0){  // we make an if loop that uses the remainder operator to divide the index by two, and if there is a remainder the of any number not 0 then the if loop does not run. Only equal numbers have a remainder of 0
     console.log(i);
   }
 }
